8191727239ead19ca0db11585cb8f76d4ddaa37f
Guy <orcabasic@gmail.com>
2023-02-04 12:24:58 +0200

    v1-bug-fix

---
+++ src/components/moon-day/moon-day-preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class='moon-day-preview'>
+    I am moon-day-preview
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'moon-day-preview',
+  props: {},
+  data() {
+      return {
+         
+      };
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  created() {},
+  components: {
+
+  },
+}
+</script>
--- src/views/moon-cal.vue
+++ src/views/moon-cal.vue
@@ -1,51 +1,48 @@
 <template>
-  <section class='moon-cal'>
-      <select v-model="selectedLocation" class="form-item">
-        <option value="" disabled />
-        <option
-          v-for="location in allLocations"
-          :key="location._id"
-          :value="location"
-          :lable="location.title"
-        >
-          {{ location.title }}
-        </option>
-      </select>
-      <section v-for="day in moonCal" :key="day.date" class="moon-cal-days">
-        {{day.date}}
-      </section>
+  <section class="moon-cal">
+    <select v-model="selectedLocation" class="form-item">
+      <option value="" disabled />
+      <option
+        v-for="location in allLocations"
+        :key="location._id"
+        :value="location"
+        :lable="location.title"
+      >
+        {{ location.title }}
+      </option>
+    </select>
+    <section v-for="day in moonCal" :key="day.date" class="moon-cal-days">
+      {{ day.date }}
+    </section>
   </section>
 </template>
 
 <script>
-import {moonCalService} from '../services/moonCalService'
+import { moonCalService } from "../services/moonCalService";
+import moonDayPreview from "../components/moon-day/moon-day-preview.vue";
 
 export default {
-  name: 'moon-cal',
+  name: "moon-cal",
   props: {},
   data() {
-      return {
-         selectedLocation: null,
-      };
+    return {
+      selectedLocation: null,
+    };
   },
-  methods: {
-
-  },
+  methods: {},
   computed: {
     allLocations() {
-        return this.$store.getters.allLocations;
+      return this.$store.getters.allLocations;
     },
     moonCal() {
-      if (!this.selectedLocation) this.selectedLocation = this.allLocations[0]
-      const {long, lat} = this.selectedLocation
-      return moonCalService.dateFnsCal(2023, lat, long)
-    }
+      if (!this.selectedLocation) this.selectedLocation = this.allLocations[0];
+      const { long, lat } = this.selectedLocation;
+      return moonCalService.dateFnsCal(2023, lat, long);
+    },
   },
-  created() {
-    
-  },
+  created() {},
   components: {
-
+    moonDayPreview,
   },
-}
+};
 </script>
